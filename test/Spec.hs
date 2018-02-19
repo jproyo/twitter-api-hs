188e04482a302f6fe7d3c87792a675894509a5ad
Juan Pablo Royo <juanpablo.royo@gmail.com>
2018-02-19 13:55:07 +0100

    NA

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,37 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
 module Main (main) where
 
-import           Data.Aeson                     (Value(..), object, (.=))
+import           App                       (app)
+import           Data.Aeson                (Value (..), object, (.=))
+import           Network.HTTP.Types.Header
 import           Test.Hspec
 import           Test.Hspec.Wai
 import           Test.Hspec.Wai.JSON
-import           Network.HTTP.Types.Header
-import           App                            (app)
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = with app $ do
-
+spec = with app $
   describe "GET /" $ do
-    it "responds with 200" $ do
+
+    it "responds with 200" $
       get "/" `shouldRespondWith` 200
 
-    it "get on / responds with '{endpoints:{user_timeline:/user/{userName}/timeline}}' and 200 and headers" $ do
+    it "get on / responds with '{endpoints:{user_timeline:/user/{userName}/timeline}}' and 200 and headers" $
       get "/" `shouldRespondWith` expectRootJsonResponse {matchStatus = 200} {matchHeaders = ["Content-Type" <:> "application/json; charset=utf-8"]}
-
-  -- describe "GET /user/{userName}/timeline" $ do
-  --   it "responds with tweeter timeline JSON" $ do
-  --     get "/user/someuser/timeline" `shouldRespondWith` expectTimeLineJsonResponse {matchStatus = 200}
 
 
 expectRootJsonResponse =
   let ResponseMatcher status headers body = [json|{endpoints: {user_timeline: "/user/{userName}/timeline"}}|]
   in ResponseMatcher status [hContentType <:> "application/json; charset=utf-8"] body
-
--- expectTimeLineJsonResponse =
---   let ResponseMatcher status headers body = [json|[{text: "My tweet", userName: "someuser", retweetCount: 10, favoriteCount: 1}]|]
---   in ResponseMatcher status [hContentType <:> "application/json; charset=utf-8"] body
